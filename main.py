8497bac26943f31136c1807a16c237ba49262fdb
Oleg <oleg.s.kerimov@yandex.com>
2022-07-16 02:00:46 +0300

    Finished

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
-import re
 import time
+import datetime
+from datetime import datetime
 
 arr = []
 animals = []
@@ -16,7 +17,7 @@ tests = [
              'tutor': [1594663290, 1594663430, 1594663443, 1594666473]},
      'answer': 3117
     },
-    {'data': {'lesson': [1594702800, 1594706400],
+    {'data': {'lesson': [1594702800, 1594706400], #1594702807?????????
              'pupil': [1594702789, 1594704500, 1594702807, 1594704542, 1594704512, 1594704513, 1594704564, 1594705150, 1594704581, 1594704582, 1594704734, 1594705009, 1594705095, 1594705096, 1594705106, 1594706480, 1594705158, 1594705773, 1594705849, 1594706480, 1594706500, 1594706875, 1594706502, 1594706503, 1594706524, 1594706524, 1594706579, 1594706641],
              'tutor': [1594700035, 1594700364, 1594702749, 1594705148, 1594705149, 1594706463]},
     'answer': 3577
@@ -52,7 +53,6 @@ print('Вы ввели:', arr)
 print('Введите искомое:')
 key = input()
 print('Индекс первого искомого элемента:', find(arr, key))
-time.sleep(3)
 
 print('')
 print('---------------------------------Задание 2--------------------------------')
@@ -93,7 +93,6 @@ for animal in animals:
 for k, v in sorted(symbols.items()):
   print(f"Буква {k}: {v}")
 time.sleep(5)
-
 print('')
 print('---------------------------------Задание 3--------------------------------')
 
@@ -103,97 +102,94 @@ def appearance(data):
     lesson = data.get('lesson')
     teacher = data.get('tutor')
     student = data.get('pupil')
-    print('Lesson', lesson)
-    print('Teacher', teacher)
-    print('Student:', student)
-    if lesson[0] > teacher[0]:
-        print('Учитель зашел раньше начала урока')
-        print(lesson[0], '>', teacher[0])
-        if teacher[1] > lesson[0]:
-            print('Учитель зашел раньше начала урока и не выходил до его начала')
-            print(lesson[0], '>', teacher[0])
-            prom = (teacher[1] - teacher[0]) - (lesson[0] - teacher[0])
-            new_start = teacher[1] - prom
-            teacher[0] = new_start
-        elif teacher[1] < lesson[0]:
-            print('Учитель зашел и вышел раньше начала урока')
-            print(teacher[1], '<', lesson[0])
-            teacher.pop(0)
-            teacher.pop(0)
-    if teacher[-1] > lesson[1]:
-        print('Учитель вышел после конца урока')
-        print(teacher[-1], '>', lesson[1])
-        if teacher[-2] > lesson[1]:
-            print('Учитель зашел и вышел после конца урока')
-            print(teacher[-2], '>', lesson[1])
-            teacher.pop(-1)
-            teacher.pop(-1)
-        elif lesson[1] > teacher[-2]:
-            print('Учитель зашел до конца урока и вышел после конца урока')
-            print(lesson[1], '>', teacher[-2])
-            #prom = (teacher[-1] - teacher[-2]) - (teacher[-1] - lesson[1])
-            teacher[-1] = lesson[1]
-        elif lesson[1] > teacher[-1]:
-            print('Учитель зашел и вышел до конца урока')
-            print(lesson[1], '>', teacher[-1])
-    print('Teacher', teacher)
-    print('Student:', student)
-    print('')
-    print('')
-    print('')
+    teacher.sort()
+    student.sort()
+    teacher[0] = lesson[0] if lesson[0] > teacher[0] else teacher[0]
+    teacher[-1] = lesson[-1] if lesson[-1] < teacher[-1] else teacher[-1]
+    new_teacher = []
+    new_student = []
+    new_lesson = []
+    new_lesson.append(datetime.fromtimestamp(lesson[0]))
+    new_lesson.append(datetime.fromtimestamp(lesson[1]))
 
-    #print('Teacher', teacher)
-    #print('Student:', student)
+    for i in range(0, len(teacher), 2):
+        if teacher[i] < lesson[1] and teacher[i+1] > lesson[0]:
+            new_teacher.append({
+                'start': datetime.fromtimestamp(teacher[i] if teacher[i] >= lesson[0] else lesson[0]),
+                'end': datetime.fromtimestamp(teacher[i+1] if teacher[i+1] <= lesson[1] else lesson[1])
+            })
 
-    for i in range(0, (len(teacher)-1)):
-        for j in range(0, (len(student)-1)):
-            if teacher[i] == student[j]: #если учитель и ученик зашли в одно время
-                if student[j+1] == teacher[i+1]: #и вышли в одно время
-                    cnt = cnt + (teacher[i+1] - teacher[i])
-                    #print('1.1', cnt)
-                elif teacher[i+1] > student[j+1]: #и ученик вышел раньше
-                    cnt = cnt + ((teacher[i+1] - teacher[i]) - (teacher[i+1] - student[j+1]))
-                    #print('1.2', cnt)
-                elif student[j+1] > teacher[i+1]: #и учитель вышел раньше
-                    cnt = cnt + ((teacher[i+1] - teacher[i]) - (student[j+1] - teacher[i+1]))
-                    #print('1.3', cnt)
-            elif teacher[i] < student[j]: #если учитель зашел на урок раньше
-                if student[j+1] == teacher[i+1]: #и вышли в одно время
-                    cnt = cnt + ((teacher[i+1] - teacher[i]) - (student[j] - teacher[i]))
-                    #print('2.1', cnt)
-                elif teacher[i + 1] > student[j + 1]:  #и ученик вышел раньше
-                    cnt = cnt + ((teacher[i+1] - teacher[i]) - (student[j] - teacher[i]) - (teacher[i+1] - student[j+1]))
-                    #print('2.2', cnt)
-                elif student[j+1] > teacher[i+1]: #и учитель вышел раньше
-                    cnt = cnt + ((teacher[i+1] - teacher[i]) - (student[i] - teacher[i]) - (student[j+1] - teacher[i+1]))
-                    #print('2.3', cnt)
-            elif teacher[i] > student[j]: #если ученик зашел раньше учителя
-                if teacher[i+1] == student[j+1]: #и вышли в одно время
-                    cnt = cnt + ((teacher[i+1] - teacher[i]) - (teacher[i] - student[j]))
-                    #print('3.1', cnt)
-                elif student[j+1] < teacher[i + 1]:  # и ученик вышел раньше
-                    cnt = cnt + ((student[j] - student[j + 1]) - (teacher[i] - student[j]) - (teacher[i + 1] - student[j + 1]))
-                    #print('3.2', cnt)
-                elif student[j+1] > teacher[i+1]: #и учитель вышел раньше
-                    cnt = cnt + ((student[j] - student[j + 1]) - (teacher[i] - student[j]) - (student[j + 1] - teacher[i + 1]))
-                    #print('3.3', cnt)
-    #print('Cnt', cnt)
-    return cnt
+    for i in range(0, len(student), 2):
+        if student[i] < lesson[1] and student[i+1] > lesson[0]:
+            new_student.append({
+                #'start': datetime.fromtimestamp(student[i] if student[i] <= lesson[1] else lesson[1]),
+                'start': datetime.fromtimestamp(student[i] if student[i] >= lesson[0] else lesson[0]),
+                'end': datetime.fromtimestamp(student[i+1] if student[i+1] <= lesson[1] else lesson[1])
+            })
+    if len(new_teacher) > len(new_student):
+        intervals = list(new_teacher)
+        big_intervals = [
+            i for i in new_student if i['start'] >= intervals[0]['start'] or i['end'] <= intervals[-1]['end']
+        ]
+    else:
+        intervals = list(new_student)
+        big_intervals = [
+            i for i in new_teacher if i['start'] >= intervals[0]['start'] or i['end'] <= intervals[-1]['end']
+        ]
+    new_intervals = []
 
-
-    #global lesson_time, teacher_time, student_time
-    #student = data.get('pupil')
-    #for i in range(0, (len(student)-1)):
-     #   student_time = student_time + (student[i+1] - student[i])
-    #print('Student time:', student_time)
-    #teacher = data.get('tutor')
-    #for i in range(0, (len(teacher) - 1)):
-     #   teacher_time = teacher_time + (student[i + 1] - student[i])
-    #print('Teacher time', teacher_time)
-    #return(student_time, teacher_time)
+    for i in range(0, len(intervals)):
+        for j in range(0, len(big_intervals)):
+            a = intervals[i]['start']
+            b = intervals[i]['end']
+            c = big_intervals[j]['start']
+            d = big_intervals[j]['end']
+            if c == a:
+                if d == b:
+                    prom = a - b
+                    sec = prom.total_seconds()
+                    new_intervals.append(sec)
+                elif d < b:
+                    prom = (b - a) - (b - d)
+                    sec = prom.total_seconds()
+                    new_intervals.append(sec)
+                elif d > b:
+                    prom = (d - c) - (d - b)
+                    sec = prom.total_seconds()
+                    new_intervals.append(sec)
+            elif b == d:
+                if c > a:
+                    prom = (b - a) - (c - a)
+                    sec = prom.total_seconds()
+                    new_intervals.append(sec)
+                elif a > c:
+                    prom = (d - c) - (a - c)
+                    sec = prom.total_seconds()
+                    new_intervals.append(sec)
+            elif a > c:
+                if d > b:
+                    prom = (d - c) - ((a - c) + (d - b))
+                    sec = prom.total_seconds()
+                    new_intervals.append(sec)
+                elif a < d < b:
+                    prom = (d - c) - (a - c)
+                    sec = prom.total_seconds()
+                    new_intervals.append(sec)
+            elif c > a:
+                if d < b:
+                    prom = (b - a) - ((c - a) + (b - d))
+                    sec = prom.total_seconds()
+                    new_intervals.append(sec)
+                elif c < b < d:
+                    prom = (d - c) - (d - b)
+                    sec = prom.total_seconds()
+                    new_intervals.append(sec)
+            elif a >= d or c >= b:
+                continue
+    return sum(new_intervals)
 
 
 for i, test in enumerate(tests):
   test_answer = appearance(test['data'])
-  #print(test_answer)
+  print(test_answer)
 
